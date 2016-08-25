68ccc93cbe0307b27a5735bfe5db08a139d36c9c
MarkKrauss <markuskrauss@ymail.com>
2016-08-25 15:06:38 +0200

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,18 @@
+import source.MVC.view.SwingEditor;
+
+/**
+ * Einstiegspunkt des Editors
+ *                                          
+ * @author Markus Krauss
+ * @version 0.1
+ */      
+public class MainClass {
+   /**
+    * Erstellen der Instanz vom SwingEditor
+    *
+    * @param args
+    */
+   public static void main(String[] args) {
+      SwingEditor s = new SwingEditor();
+   }
+}
